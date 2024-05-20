736c09b1e8c5de6f548fef5b1d8fad5afc4fdb20
ProgrammingOperative <wachura11t@gmail.com>
2024-05-20 12:18:17 +0300

    lambda complete

--- lambda_package/lambda_function.py
+++ lambda_package/lambda_function.py
@@ -1,33 +1,41 @@
-import json
 import os
+import json
 import boto3
 from kafka import KafkaProducer
+from time import sleep
 
 # Kafka broker's IP address and port
-bootstrap_servers = '3.147.45.97:9092'
+bootstrap_servers = '3.141.4.193:9092'
 
-# Kafka topic to produce to
-topic = 'demo_testing2'
-
-# Create Kafka producer
+# Create a Kafka producer
 producer = KafkaProducer(bootstrap_servers=bootstrap_servers)
 
 def lambda_handler(event, context):
-    # Get the uploaded file details
     s3 = boto3.client('s3')
+    
     for record in event['Records']:
         bucket_name = record['s3']['bucket']['name']
         object_key = record['s3']['object']['key']
         file_name = os.path.basename(object_key)
 
         # Download the file from S3
-        download_path = '/tmp/{}'.format(file_name)
-        s3.download_file(bucket_name, object_key, download_path)
+        download_path = f'/tmp/{file_name}'
+        
+        try:
+            s3.download_file(bucket_name, object_key, download_path)
+            print(f"Downloaded {object_key} from {bucket_name} to {download_path}")
+        except Exception as e:
+            print(f"Error downloading file from S3: {e}")
+            raise
 
         # Read the file and send it to Kafka
         with open(download_path, 'rb') as file:
-            producer.send(topic, file.read())
-    
+            image_data = file.read()
+            producer.send('demo_testing2', value=image_data)
+            producer.flush()
+            print("Image sent successfully to Kafka topic")
+            sleep(1)  # Optional: control the rate of sending messages
+
     return {
         'statusCode': 200,
         'body': json.dumps('Image sent to Kafka topic successfully!')
---
+++ lambda_package/package/lambda_function.py
@@ -0,0 +1,34 @@
+import json
+import os
+import boto3
+from kafka import KafkaProducer
+
+# Kafka broker's IP address and port
+bootstrap_servers = '18.222.121.231:9092'
+
+# Kafka topic to produce to
+topic = 'demo_testing2'
+
+# Create Kafka producer
+producer = KafkaProducer(bootstrap_servers=bootstrap_servers)
+
+def lambda_handler(event, context):
+    # Get the uploaded file details
+    s3 = boto3.client('s3')
+    for record in event['Records']:
+        bucket_name = record['s3']['bucket']['name']
+        object_key = record['s3']['object']['key']
+        file_name = os.path.basename(object_key)
+
+        # Download the file from S3
+        download_path = '/tmp/{}'.format(file_name)
+        s3.download_file(bucket_name, object_key, download_path)
+
+        # Read the file and send it to Kafka
+        with open(download_path, 'rb') as file:
+            producer.send(topic, file.read())
+    
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Image sent to Kafka topic successfully!')
+    }
